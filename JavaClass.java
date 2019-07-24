eaa80c3a836950128d03400f7fe481b0a9be4c00
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-07-24 17:40:01 -0400

    Create JavaClass.java

---
+++ JavaClass.java
@@ -0,0 +1,24 @@
+package demofortoday;
+
+public class JavaClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		addTwoNumbers();
+		addTwoNumbers();
+
+	}
+
+	public static void addTwoNumbers() {
+
+		int x = 22;
+		int y = 55;
+		int z = 88;
+		int h = x + y + z;
+
+		System.out.println(h);
+
+	}
+
+}
