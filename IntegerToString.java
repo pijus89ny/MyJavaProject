4a4b0386d0ba55fcb41adb2c77aaf23e1d364a0b
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-07-31 20:25:48 -0400

    Create IntegerToString.java

---
+++ IntegerToString.java
@@ -0,0 +1,20 @@
+package mypakage;
+
+public class IntegerToString {
+
+	public static void main(String[] args) {
+		// int to String
+		
+		int number = 100;
+		
+		
+	String converted = Integer.toString(number);
+	
+	System.out.println(converted);
+	
+	System.out.println(converted+100);
+	
+
+	}
+
+}
