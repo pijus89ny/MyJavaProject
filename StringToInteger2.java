0258e79d0d84247bbf33d7a21081ca42a9f3793a
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-01 13:18:45 -0400

    Create  StringToInteger2.java

---
+++ StringToInteger2.java
@@ -0,0 +1,23 @@
+package mypakage;
+
+public class StringToInteger {
+
+	public static void main(String[] args) {
+		// converts String to Integer
+		
+		
+		Integer work =100;
+		String myclass= work.toString();
+		System.out.println(myclass);
+		System.out.println(myclass+111);
+		
+                   ///   
+		Integer my=200;
+		String phone=my.toString();
+		System.out.println(phone);
+		System.out.println( phone+111);
+		
+		
+	}
+
+}
