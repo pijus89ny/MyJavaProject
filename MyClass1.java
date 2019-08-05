2136c42f5f4d27a122377dd11ae52dabfc301008
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-05 02:06:06 -0400

    Create  MyClass1.java

---
+++ MyClass1.java
@@ -0,0 +1,36 @@
+package mypakage;
+
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Integer myInt = 5;
+		Double myDouble=3445.2112;
+		Character myChar='q';
+		Float myFloat=234f;
+		Long myLong=12283459776L;
+		
+	
+		
+		System.out.println(myInt.intValue());
+		System.out.println(myDouble.doubleValue());
+		System.out.println(myChar.charValue());
+		
+			
+		System.out.println(myFloat.floatValue());
+		System.out.println(myLong.longValue());
+		
+		
+		
+	
+	
+		
+		
+		
+		
+		
+	}
+
+}
