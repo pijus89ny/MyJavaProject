db0bd3ad4f5dd061bfca7b66333495d9f23cf3b8
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-06 19:51:11 -0400

    Create StkDemo.java

---
+++ StkDemo.java
@@ -0,0 +1,27 @@
+package partice;
+
+import java.util.Stack;
+
+public class StkDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		Stack<String>stk=new Stack<String>();
+		
+		
+		
+		stk.add("pijus");
+		stk.add("sushanta");
+		stk.add("p");
+		stk.add("jon");
+		stk.add("kona");
+		stk.add("joe");
+		stk.add("pj");
+		System.out.println(stk);
+	}
+
+}
