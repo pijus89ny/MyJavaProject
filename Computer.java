c67994614f2fbb1e4ec90a809aebd0f0e7bfc42b
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-15 14:36:16 -0400

    Create  Computer.java

---
+++ Computer.java
@@ -0,0 +1,49 @@
+package mypakage;
+
+public class Computer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       String by = new String("pijud");
+       System.out.println(by);
+       String ny = new  String("p");
+       System.out.println(ny);
+       String n = new String("nyc");
+       System.out.println(n);
+       String p=new String("byte");
+       System.out.println(p);
+       String my =new String("byre");
+       System.out.println(my);
+       String r=new String("roy");
+       System.out.println(r);
+       String j = new String("jfk");
+       System.out.println(j);
+       String y = new String("mondal");
+       System.out.println(y);
+       String u= new String("jon");
+       System.out.println(u);
+       String v = new String("pj");
+       System.out.println(v);
+       String xz = new String("mona");
+       System.out.println(xz);
+       String sex= new String("lol");
+       System.out.println(sex);
+       String ma= new String("manhattan");
+       System.out.println(ma);
+       String jk= new String("nyc");
+       System.out.println(jk);
+       String b=new String("pp");
+       System.out.println(b);
+       String e = new String("tipu");
+       System.out.println(e);
+       String k=new String("kol");
+       System.out.println(k);
+       String nj=new String("opu");
+       System.out.println(nj);
+       
+       
+    		   
+       
+	}
+
+}
