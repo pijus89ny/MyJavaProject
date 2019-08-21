837690e0e1f24b5877524bdc457a0b7ba817ed05
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-21 03:01:26 -0400

    Create JonClass.java

---
+++ JonClass.java
@@ -0,0 +1,15 @@
+package mypakage;
+
+public class JonClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int sy=3;
+     int zb =2;
+     int s=2;
+     int xy =5;
+     int amc=(sy+zb+s+xy);
+     System.out.println(amc);
+	}
+
+}
