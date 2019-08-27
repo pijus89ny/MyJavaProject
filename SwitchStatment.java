5826d2fc20ab3cfcdb72713c828daf4be4fd5216
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-27 00:52:33 -0400

    Create SwitchStatment.java

---
+++ SwitchStatment.java
@@ -0,0 +1,39 @@
+package mypakage;
+
+public class SwitchStatment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int dayOfTheWeek =8;
+       switch (dayOfTheWeek) {
+       
+       
+       
+       case 1:System.out.println("mon");
+       break;
+        
+       case 2: System.out.println("tues");
+       break;
+       
+       case 3: System.out.println("wednesday");
+       break;
+       
+       case 4: System.out.println("thusday");
+       break;
+       
+       case 5: System.out.println("friday");
+       break;
+       
+       case 6: System.out.println("satudary");
+       break;
+       
+       case 7: System.out.println("sunday");
+       break;
+       default: 
+    	   System.out.println("inviled");
+       break;
+       
+       }
+	}
+
+}
