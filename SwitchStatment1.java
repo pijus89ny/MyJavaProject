8d7d3a654b51a7cba23af475831bac2e27134fa7
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-27 01:06:22 -0400

    Create SwitchStatment1.java

---
+++ SwitchStatment1.java
@@ -0,0 +1,46 @@
+package mypakage;
+
+public class SwitchStatment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int color =8;
+       switch (color) {
+       
+       
+       
+       case 1:
+    	   System.out.println("black");
+       break;
+        
+       case 2: 
+    	   System.out.println("bloue");
+       break;
+       
+       case 3: 
+    	   System.out.println("green");
+       break;
+       
+       case 4: 
+    	   System.out.println("pink");
+       break;
+       
+       case 5:
+    	   System.out.println("yellow");
+       break;
+       
+       case 6: 
+    	   System.out.println("brown");
+       break;
+       
+       case 7: 
+    	   System.out.println("red");
+       break;
+       default: 
+    	   System.out.println("indiled");
+       break;
+       
+       }
+	}
+
+}
