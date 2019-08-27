6caaee7cc980b111e556b728f0cc0a7fbf9c5625
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-27 01:31:58 -0400

    Create ForEachLoop.java

---
+++ ForEachLoop.java
@@ -0,0 +1,19 @@
+package mypakage;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		
+		
+		
+		String [] nums = {"pijus", "kona", "jon", "mona", "puja"};
+		
+		for( String number : nums) {
+			System.out.println(number);
+		}
+      
+	}
+
+}
