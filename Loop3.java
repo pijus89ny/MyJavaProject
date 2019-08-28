53f058b4e364fa8f4bb4b9d067bc759b9e4257d8
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-28 02:41:49 -0400

    Create  Loop3.java

---
+++ Loop3.java
@@ -0,0 +1,28 @@
+package mypakage;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		
+		for(int h= 600; h<900; h++) {
+			System.out.println(h);
+			
+			
+			
+			
+			int  [] number = { 11, 333, };
+			
+			int sum = 0;
+			
+			for( int jon : number) {
+				System.out.println(jon);
+			}
+			
+			}
+		}
+		
+	}
+
+
