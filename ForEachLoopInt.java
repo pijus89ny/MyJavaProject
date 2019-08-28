8c89646130d696a5def76fcaac7970c45744c75f
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-28 01:57:16 -0400

    Create ForEachLoopInt.java

---
+++ ForEachLoopInt.java
@@ -0,0 +1,21 @@
+package mypakage;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		
+		
+		
+		int [] numbers = {1223, 555, 9999, 555};
+		
+		
+		for (int jon : numbers ) {
+			
+			System.out.println(jon);
+		}
+		
+		
+		
+	}
+
+}
