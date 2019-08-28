20d3701afbcbb379a09d690f9c67395af3b98741
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-28 02:06:57 -0400

    Create Loop2.java

---
+++ Loop2.java
@@ -0,0 +1,16 @@
+package mypakage;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		for(int i= 10; i<100; i++) {
+			System.out.println(i);
+		}
+		
+		
+		
+	}
+
+}
