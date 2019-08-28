6dff5ca254b8f6d628e1c0d486d75b9558bd415b
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-08-28 18:04:17 -0400

    Create  Loop4.java

---
+++ Loop4.java
@@ -0,0 +1,24 @@
+package mypakage;
+
+public class Loop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		int [] nums= {11,22,};
+		
+		for(int jon : nums) {
+			
+			System.out.println(jon);
+			
+			
+			int u=0;
+		for(int i=90; i<100; i++) {
+			System.out.println(i);
+		}
+			
+		}
+		
+	}
+
+}
