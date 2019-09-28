1845168a70d9bfc4e35e2ec46934bc30c7dc129b
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-09-28 13:50:42 -0400

    Create MyFirstCompute.java

---
+++ MyFirstCompute.java
@@ -0,0 +1,19 @@
+package mypakage;
+
+public class MyFirstCompute {
+
+	public static String myFullNmae(String firstName, String lastName) {
+		
+		String fullName=firstName+ " "+ lastName;
+		return fullName;
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println(myFullNmae("pijus", "mondal"));
+
+	}
+
+}
