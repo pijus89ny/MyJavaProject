51ed7803aabc01bd13e7cd13097f75885eae8c9f
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-09-28 12:57:21 -0400

    Create JonDemo1.java

---
+++ JonDemo1.java
@@ -0,0 +1,20 @@
+package json;
+
+public class JonDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		int p=987;
+		double k=43.754;
+		int u=(int)(p*k);
+
+		
+		System.out.println(u);
+		
+	}
+
+}
