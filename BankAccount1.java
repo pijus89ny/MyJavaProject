76fbc8e5c6d258a5467760cd1807ded47c96f7dc
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-09-28 13:23:45 -0400

    Create BankAccount1.java

---
+++ BankAccount1.java
@@ -0,0 +1,19 @@
+package classthree;
+
+public class BankAccount1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		Student v=new Student();
+		v.getSum();
+	}
+
+	void getSum() {
+		int acNumber=1;
+		String acNm="pijus";
+		double blance=123.32;
+		boolean isTrue=false;
+		System.out.println(acNumber);
+	}
+}
