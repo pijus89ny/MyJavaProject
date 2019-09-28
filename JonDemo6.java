5653c710327197d9c076566fcddef85a5362a589
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-09-28 13:11:43 -0400

    Create JonDemo6.java

---
+++ JonDemo6.java
@@ -0,0 +1,21 @@
+package json;
+
+public class JonDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+     int y=123;
+     double p=22344.43;
+	
+     int i=(int)(y*p);
+		
+		System.out.println(i);
+		
+
+		
+	}
+
+}
