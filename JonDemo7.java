4f87d9f762191c6de9fc9f94c8c1205c951b29fa
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-09-28 13:15:12 -0400

    Create JonDemo7.java

---
+++ JonDemo7.java
@@ -0,0 +1,18 @@
+package json;
+
+public class JonDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+     int anotherInt=12043;
+     byte anotherByte=(byte)anotherInt;
+     System.out.println(anotherByte);
+		
+
+		
+	}
+
+}
