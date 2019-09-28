b2cd672e271c6918da6b9fbff11203a94687c23b
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-09-28 13:18:53 -0400

    Create JonDemo8.java

---
+++ JonDemo8.java
@@ -0,0 +1,18 @@
+package json;
+
+public class JonDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+  byte a=127;
+  int d=a;
+  System.out.println(d);
+		
+
+		
+	}
+
+}
