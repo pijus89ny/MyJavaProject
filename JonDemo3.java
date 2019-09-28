0c4cd3a3bc97b42d29bf10fea827d297ee2559d4
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-09-28 13:05:10 -0400

    Create JonDemo3.java

---
+++ JonDemo3.java
@@ -0,0 +1,20 @@
+package json;
+
+public class JonDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		float o=123.321f;
+		int p=2;
+		
+		float result=(o*p);
+		System.out.println(result);
+		
+
+		
+	}
+
+}
