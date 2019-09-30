cf0e692c2db727afccdb5b48030505415bdbed32
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-09-30 13:53:19 -0400

    Create MyFirstComputer2.java

---
+++ MyFirstComputer2.java
@@ -0,0 +1,14 @@
+package mypakage;
+
+public class MyFirstCompute {
+
+	public static boolean compareTwoString(String c, String b) {
+		boolean decision=c.equals(b);
+		return decision;
+	}
+	public static void main(String[] args) {
+		System.out.println(compareTwoString("pijus", "pijus1"));
+
+	}
+
+}
