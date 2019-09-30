44c502ada70b81c13d810c0fd404cf3373546030
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-09-30 13:23:50 -0400

    Create WeAreBoredToday11.java

---
+++ WeAreBoredToday11.java
@@ -0,0 +1,21 @@
+package demofortoday;
+
+public class WeAreBoredToday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		addTwoNumbers();
+		addTwoNumbers();
+
+	}
+
+	public static void addTwoNumbers() {
+
+        String x="we are go home. we need to work";
+
+		System.out.println(x);
+
+	}
+
+}
