109d19df777b8675ae5c9277fa69efdbe7033cfc
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-10-01 01:26:40 -0400

    Create ClassJon.java

---
+++ ClassJon.java
@@ -0,0 +1,26 @@
+package mypakage;
+
+public class ClassJon {
+	public static int add(int a, int b) {
+		int result;
+		result = a*b;
+		return result;
+	}
+	public static void main(String[] args) {
+		
+		
+		int x=add(12,12);
+		System.out.println(x);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
