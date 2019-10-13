384e903ff5742fc2b2ab6297f933f7c8c957edfd
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-10-13 03:09:15 -0400

    Create workproperly1.java

---
+++ workproperly1.java
@@ -0,0 +1,14 @@
+package classThreejon;
+
+public class workproperly {
+
+	public static void main(String[] args) {
+	
+		workproperly ny=new workproperly();
+	}
+	
+	public  workproperly() {
+		System.out.println("i am a pijus");
+	}
+
+}
