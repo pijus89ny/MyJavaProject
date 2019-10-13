5dfb9974ad7cbcff05452b071fd9202c9e93ffc5
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-10-13 02:59:30 -0400

    Create workproperly.java

---
+++ workproperly.java
@@ -0,0 +1,22 @@
+package classThreejon;
+
+public class workproperly {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println(gumSumNumbers(22,33,44,55,66,77,88,99));
+		
+		
+		
+		
+		
+	
+	}
+
+	private static int gumSumNumbers(int i, int j, int k, int l, int m, int n, int o, int p) {
+		int sum=i+j+k+l+m+n+o+p;
+		return sum;
+	}
+
+}
