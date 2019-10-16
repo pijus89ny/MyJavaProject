27e2bc79bbc2f56d8e6bef3ac4a76a9f30372614
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-10-16 00:21:31 -0400

    Create workproperly11.java

---
+++ workproperly11.java
@@ -0,0 +1,16 @@
+package classThreejon;
+
+public class workproperly {
+   static int ny=12;
+   int nj=13;
+
+	public static void main(String[] args) {
+ 
+		workproperly joy = new workproperly();
+		int yo=33;
+		System.out.println(yo);
+		System.out.println(joy.nj);
+	
+    
+	}
+}
