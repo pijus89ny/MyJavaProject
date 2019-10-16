c4a56c9ee95aa128f5c32ff92e7d59c6ef32dd8f
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-10-16 01:19:03 -0400

    Create ITooWeek2.java

---
+++ ITooWeek2.java
@@ -0,0 +1,14 @@
+package gohome;
+
+public class ITooWeek {
+
+	public static void main(String[] args) {
+		
+		ITooWeek ny=new ITooWeek();
+
+	}
+	
+	public static void ITooWeek() {
+		System.out.println("i don,t know anything");
+	}
+}
