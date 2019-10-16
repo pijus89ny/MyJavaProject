30e808443dfa4e67da9f6e0417924e412fdb028d
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-10-16 00:38:24 -0400

    Create  workproperly2.java

---
+++ workproperly2.java
@@ -0,0 +1,15 @@
+package classThreejon;
+
+public class workproperly {
+   
+
+	public static void main(String[] args) {
+ 
+     System.out.println(getThreeNumbers(89,10,44,66,77,88,90));
+	}
+
+	private static int getThreeNumbers(int i, int j, int k, int l, int m, int n, int o) {
+		int ny=i+j+k+l+m+n+o;
+		return ny;
+	}
+}
