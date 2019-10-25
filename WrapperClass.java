b00507d1c0a822f5785c8b71c4da329899e2af69
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-10-25 02:27:22 -0400

    Create WrapperClass.java

---
+++ WrapperClass.java
@@ -0,0 +1,20 @@
+package jon;
+
+
+
+public class WrapperClass {
+   
+    	
+    	
+    
+	public static void main(String[] args) {
+		
+        Integer ny=100;
+        String myString=ny.toString();
+        System.out.println(myString);
+        System.out.println(myString+100);
+
+		
+	}   
+
+}
