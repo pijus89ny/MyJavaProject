bf624ab633a21abe31af21df9163f7d9f8be6ba3
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-10-29 02:55:03 -0400

    Create  KmFromMile.java

---
+++ KmFromMile.java
@@ -0,0 +1,17 @@
+package mypakage;
+
+public class KmFromMile {
+
+	
+public static double KmFromMile(double mile) {
+	double n=mile*1.61;
+	return n;
+}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println(KmFromMile(1.2));
+	}
+
+}
