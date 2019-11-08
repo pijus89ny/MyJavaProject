1e95287e20c270e2fa6709b9a8f355ef75e86608
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-11-08 03:07:40 -0500

    Create  PetoRego int.java

---
+++ PetoRego int.java
@@ -0,0 +1,19 @@
+package jon;
+
+
+
+public class PetoRego {
+              public static int add(int a,int c) {
+            	  int pijus;
+            	  pijus=a+c;
+            	  return pijus;
+              }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int p=add(11,11);
+        System.out.println(p);
+		
+		
+	}
+
+}
