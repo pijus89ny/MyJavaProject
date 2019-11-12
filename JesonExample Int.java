ccf0afbc5b56e606f86d30274ac2dc8aaac895a5
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-11-12 01:30:37 -0500

    Create JesonExample Int.java

---
+++ JesonExample Int.java
@@ -0,0 +1,22 @@
+package jon.demo;
+
+public class JesonExample {
+
+	public static void main(String[] args) {
+		
+		
+		double p=123.432;
+		int k=123;
+		int l=(int)(p*k);
+		System.out.println(l);
+		
+		
+		
+		
+		
+		
+		
+            
+	}
+
+}
