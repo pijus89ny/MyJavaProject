70ee7e94bdf40acd9a0e1fa4522c2474371960a9
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-11-12 02:24:25 -0500

    Create  ClassY  extends ClassX.java

---
+++ ClassY  extends ClassX.java
@@ -0,0 +1,12 @@
+package oopsdemo;
+
+public class ClassY  extends ClassX{
+
+	public static void main(String[] args) {
+		
+		 ClassY ny = new  ClassY();
+		 ny.sleep();
+		 System.out.println(ny.d);
+	}
+
+}
