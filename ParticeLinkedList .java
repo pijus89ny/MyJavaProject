f0db83bc151570af18c77c6a3feb45e391a028cf
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-11-15 01:48:16 -0500

    Create ParticeLinkedList .java

---
+++ ParticeLinkedList .java
@@ -0,0 +1,20 @@
+package linklistdemo;
+
+import java.util.Vector;
+
+public class ParticeLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         
+		Vector ny=new Vector (3,3);
+		
+		ny.add(1, "pijus");
+		ny.add(2, "mona");
+		ny.add(3, "kona");
+		ny.add(4, "jon");
+		ny.add(5, "mon");
+		ny.add(6, "prosonjit");
+		ny.add(7, "mondal");
+		System.out.println(ny);
+		
