ab8a1350d5ed6773dbff51e6fee1668777c18aff
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2019-11-25 02:53:13 -0500

    Create TdBankAccountStartsWith.java

---
+++ TdBankAccountStartsWith.java
@@ -0,0 +1,16 @@
+package sushanta;
+
+
+
+public class TdBankAccount {
+
+	public static void main(String[] args) {
+          
+		
+		
+		
+		String city="new york";
+		System.out.println(city.startsWith("ne"));
+       
+	}
+	}
