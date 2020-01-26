f7fc5974873a2cc11f163e9050b86efbbd4195a7
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2020-01-26 02:27:28 -0500

    Create  Integer,Double,Float,Long.java

---
+++ Integer,Double,Float,Long.java
@@ -0,0 +1,19 @@
+package sushanta;
+
+
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+	
+		Integer myInt=5;
+		Double myDouble=7.98;
+		Float myFloat=234.54f;
+		Long myLong=123678L;
+		System.out.println(myFloat.floatValue());
+		System.out.println(myLong.longValue());
+		System.out.println(myInt.intValue());
+		System.out.println(myDouble.doubleValue());
+		
+}
+}
