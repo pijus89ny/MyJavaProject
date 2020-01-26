1ee5583379ed8f06cd09171c1386e46c2fa1b5d2
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2020-01-26 02:36:53 -0500

    Create Integer,int,String.java

---
+++ Integer,int,String.java
@@ -0,0 +1,17 @@
+package sushanta;
+
+
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+	
+		Integer b=666;
+		String vy=b.toString();
+		System.out.println(vy+666);
+		String hk="100";
+		int ct=Integer.valueOf(hk);
+		System.out.println(ct);
+		
+}
+}
