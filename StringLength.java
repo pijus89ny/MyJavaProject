40686fde6d90c249c0fde39d9f8c7bb9c7471018
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2020-01-26 02:54:24 -0500

    Create StringLength.java

---
+++ StringLength.java
@@ -0,0 +1,13 @@
+package sushanta;
+
+
+
+public class QueueDemo {
+
+	public static void main(String[] args) {
+	
+		String g="pijus go home today";
+		int ny=g.length();
+		System.out.println(ny);
+}
+}
