f8d1ae78a56b8b09e4dd01d8f18c67f277b8b891
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2020-02-02 03:11:05 -0500

    Create StringArrays.java

---
+++ StringArrays.java
@@ -0,0 +1,15 @@
+package sushanta;
+
+import java.util.Arrays;
+
+public class PjB {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+		String [] ny= {"pijus","kona","jon"};
+		System.out.println(Arrays.toString(ny));
+      
+	}
+
+}
