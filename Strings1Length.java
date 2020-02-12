dbe8b7f475b6e827bc935c5859c1bcf97cf4547a
pijus89ny <53278966+pijus89ny@users.noreply.github.com>
2020-02-12 03:32:53 -0500

    Create Strings1Length.java

---
+++ Strings1Length.java
@@ -0,0 +1,15 @@
+package sushanta;
+
+public class VectorClass {
+	
+	 public static void main(String[] args) {
+	
+	   
+    String s1=new String("jackson heigtht new york");
+    String s2=new String("at woodside");
+   System.out.println("String length:");
+   System.out.println(s1.length());
+   System.out.println("String length:");
+   System.out.println(s1.length());
+	    
+		}}
